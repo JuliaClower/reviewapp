72a2a89c8b5a4447fc30d614b7ff85f9771daa53
Julia Clower <juliavanclower@gmail.com>
2021-03-01 09:23:36 -0500

    added controller methods for showing results by lender and receiving URL

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,28 @@
+# frozen_string_literal: true
+
+require 'review_url_api'
 class ReviewsController < ApplicationController
   def index
     @reviews = Review.all
 
     render json: @reviews
+  end
+
+  def reviews_by_lender
+    @reviews = Review.where(lender_name: params[:lender_name].to_s)
+
+    render json: @reviews
+  end
+
+  def get_info
+    ReviewUrlApi.get_reviews(url_param)
+
+    render json: Review.all.count
+  end
+
+  private
+
+  def url_param
+    params.require(:url)
   end
 end
