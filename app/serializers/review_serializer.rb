86b994fdcb86b72fd2ea61c15e78910aec6a37d0
Julia Clower <juliavanclower@gmail.com>
2021-03-01 09:21:58 -0500

    serializer added for postman use

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,4 +1,5 @@
 class ReviewSerializer < ActiveModel::Serializer
   attributes :id, :title, :content, :star_rating, :date_of_review, :author,
+             :author_location, :lender_name,
              :created_at, :updated_at
 end
