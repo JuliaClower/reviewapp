1f900752e9cf33481ad64488e728ec52cb25c39b
Julia Clower <juliavanclower@gmail.com>
2021-03-01 09:22:36 -0500

    added routes and non-route handling

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/reviews', to: 'reviews#index'
+  get '/reviews/get_info', to: 'reviews#get_info'
+  get 'reviews/reviews_by_lender', to: 'reviews#reviews_by_lender'
+
+  match '*unmatched', to: 'application#route_not_found', via: :all
 end
