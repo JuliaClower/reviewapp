927ad613b1b9e0ae49b2ff83ee5cd5e3e03839fa
Julia Clower <juliavanclower@gmail.com>
2021-03-01 09:21:44 -0500

    added a validation for lender_name requirement

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,12 @@
+require 'validation_error'
 class Review < ApplicationRecord
+  before_validation :ensure_lender_name
+
+  private
+
+  def ensure_lender_name
+    return unless lender_name.nil?
+
+    raise ::ValidationError, message: 'Lender name is required.'
+  end
 end
