00379a343e1bcbb716798b2f3495bc070c378cc6
Julia Clower <juliavanclower@gmail.com>
2021-03-01 09:21:34 -0500

    error handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  rescue_from ActionController::ParameterMissing, with: :invalid_param
+
+  def route_not_found
+    render json: '404 Not Found', status: :not_found
+  end
+
+  def invalid_param
+    render json: '400 Bad Request due to Missing Parameter', status: :bad_request
+  end
 end
